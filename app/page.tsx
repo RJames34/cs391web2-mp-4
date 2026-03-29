e86701fa4142169b6f592236586a61790cff105e
Rianna James <rjames34@bu.edu>
2026-03-28 22:49:26 -0400

    Change height from 100px to 100% in page styles

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const StyledDiv = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100px;
+    height: 100%;
     font-size: 30px;
 `;
 
