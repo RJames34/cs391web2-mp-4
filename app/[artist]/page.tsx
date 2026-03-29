0fab49d92bf739a6eff3e21cc0a2fbfec1bb2d34
Rianna James <rjames34@bu.edu>
2026-03-28 23:32:18 -0400

    Increase font size from 20px to 30px

--- app/[artist]/page.tsx
+++ app/[artist]/page.tsx
@@ -13,7 +13,7 @@ const ArtistContentWrapper = styled.main`
     align-items: center;
     width: 80vw;
     margin: auto;
-    font-size: 20px;
+    font-size: 30px;
 `;
 
 const SongsContainer = styled.div`
